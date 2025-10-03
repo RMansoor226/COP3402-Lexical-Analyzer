7676757935a578e34b5975d16a098badc7dd4300
RMansoor226 <148977591+RMansoor226@users.noreply.github.com>
2025-10-03 11:01:22 -0400

    Add files via upload
    Have solved file parsing problem and have implemented some tokenization logic. Need to complete tokenization logic now.

--- lex.c
+++ lex.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
 
+typedef enum {
+    skipsym = 1 , // Skip / ignore token
+    identsym , // Identifier
+    numbersym , // Number
+    plussym , // +
+    minussym , // -
+    multsym , // *
+    slashsym , // /
+    eqsym , // =
+    neqsym , // <>
+    lessym , // <
+    leqsym , // <=
+    gtrsym , // >
+    geqsym , // >=
+    lparentsym , // (
+    rparentsym , // )
+    commasym , // ,
+    semicolonsym , // ;
+    periodsym , // .
+    becomessym , // :=
+    beginsym , // begin
+    endsym , // end
+    ifsym , // if
+    fisym , // fi
+    thensym , // then
+    whilesym , // while
+    dosym , // do
+    callsym , // call
+    constsym , // const
+    varsym , // var
+    procsym , // procedure
+    writesym , // write
+    readsym , // read
+    elsesym , // else
+    evensym // even
+} TokenType ;
 
-
-int main(void) {
+int main(int argc, char* argv[]) {
     /*
     Pseudocode for Lexical Analyzer
     Step 1. Initialize keywords, operators, and delimiters
-        typedef enum {
-        skipsym = 1 , // Skip / ignore token
-        identsym , // Identifier
-        numbersym , // Number
-        plussym , // +
-        minussym , // -
-        multsym , // *
-        slashsym , // /
-        eqsym , // =
-        neqsym , // <>
-        lessym , // <
-        leqsym , // <=
-        gtrsym , // >
-        geqsym , // >=
-        lparentsym , // (
-        rparentsym , // )
-        commasym , // ,
-        semicolonsym , // ;
-        periodsym , // .
-        becomessym , // :=
-        beginsym , // begin
-        endsym , // end
-        ifsym , // if
-        fisym , // fi
-        thensym , // then
-        whilesym , // while
-        dosym , // do
-        callsym , // call
-        constsym , // const
-        varsym , // var
-        procsym , // procedure
-        writesym , // write
-        readsym , // read
-        elsesym , // else
-        evensym // even
-        } TokenType ;
-
     Step 2. Read the source code file
         a. Skip comments
             i. Skip single-line comments (//)
@@ -62,5 +60,86 @@ int main(void) {
         Step 6.2. Print Token List to console
     Step 7. Terminate
     */
+    FILE *inputFile = fopen(argv[1], "r");
+
+    if (!inputFile) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    char info[100];
+    int i = 0;
+    int j = 0;
+    while ((j = fgetc(inputFile)) != EOF && i < sizeof(info) - 1) {
+        if ((char) j == ' ' || (char) j == '\t' || (char) j == '\n') {
+            continue; // Skip whitespace
+            i++;
+        }
+
+        info[i++] = (char) j;
+    }
+
+
+    info[i] = '\0'; // Null-terminate the string
+
+    char tokens[100];
+    int t = 0;
+    for (int k=0; k<i; k++) {
+        printf("Index %d: %c \n", k,  info[k]);
+
+        switch (info[k]) {
+            case '+':
+                tokens[t] = plussym;
+                t++;
+                break;
+            case '-':
+                tokens[t] = minussym;
+                t++;
+                break;
+            case '*':
+                tokens[t] = multsym;
+                t++;
+                break;
+            case '/':
+                tokens[t] = slashsym;
+                t++;
+                break;
+            case '=':
+                tokens[t] = eqsym;
+                t++;
+                break;
+            case '<':
+                tokens[t] = lessym;
+                t++;
+                break;
+            case '>':
+                tokens[t] = gtrsym;
+                t++;
+                break;
+            case '(':
+                tokens[t] = lparentsym;
+                t++;
+                break;
+            case ')':
+                tokens[t] = rparentsym;
+                t++;
+                break;
+            case ',':
+                tokens[t] = commasym;
+                t++;
+                break;
+            case ';':
+                tokens[t] = semicolonsym;
+                t++;
+                break;
+            case '.':
+                tokens[t] = periodsym;
+                t++;
+                break;
+        }
+    }
+
+    fclose(inputFile);
+
     return 0;
 }
